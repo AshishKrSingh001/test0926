c50e947a4fce046fd1c94a58b030b3b3b31466b7
Ashish <ashish855116@gmail.com>
2024-02-03 21:51:27 +0530

    Uploading JAVA project that can be used for file arrangement.

--- PYTHON/a22_fromImportNumberModule.py
+++
@@ -1,6 +0,0 @@
-#from a22_NumberModule import sum_of_digit
-from a22_NumberModule import *
-
-print(sum_of_digit(123654))
-print(count_of_digit(123654))
-print(rev_of_digit(123654))
--- PYTHON/x.py
+++
@@ -1,8 +0,0 @@
-import os
-file = input("Enter file name: ")
-if os.path.exists(file):
-    os.remove(file)
-    print("FIle deleted successfully")
-else:
-    print("File not exists")
-    
